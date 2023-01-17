cbb1d5bd132a1f012be77f3e618ecabfcec13e26
Paul Auerbach <paul.auerbach@barkhauseninstitut.org>
2023-01-17 22:27:02 +0100

    Now compiling for ros2 no tests done yet

--- imu_transformer/src/imu_transformer_node.cpp
+++ imu_transformer/src/imu_transformer_node.cpp
@@ -1,13 +1,14 @@
-#include "ros/ros.h"
-#include "nodelet/loader.h"
+#include <rclcpp/rclcpp.hpp>
+#include <imu_transformer/imu_transformer.hpp>
+
+#include <memory>
 
 int main(int argc, char **argv){
-  ros::init(argc, argv, "imu_transformer");
-  nodelet::Loader nodelet;
-  nodelet::M_string remap(ros::names::getRemappings());
-  nodelet::V_string nargv;
-  std::string nodelet_name = ros::this_node::getName();
-  nodelet.load(nodelet_name, "imu_transformer/imu_transformer_nodelet", remap, nargv);
-  ros::spin();
+  rclcpp::init(argc, argv);
+
+  const rclcpp::NodeOptions options;
+
+  rclcpp::spin(std::make_shared<imu_transformer::ImuTransformer>(options));
+  rclcpp::shutdown();
   return 0;
 }
